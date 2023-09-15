ba0c2b46cbff14d4e9056f23d2942d901fa5eba6
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-15 09:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let x=sampleObject.keys();
+	(for let i=0;i<x.length;i++){
+		if(key==x[i]){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
